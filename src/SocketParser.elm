38afc30473e094039d9fccca099b7c037620dc0e
Axel Tetzlaff <axel.tetzlaff@shopify.com>
2022-07-09 21:44:49 -0400

    Pattern parsing protoype

--- src/SocketParser.elm
+++ src/SocketParser.elm
@@ -1,6 +1,9 @@
 module SocketParser exposing
     ( Fragment(..)
+    , Pattern
+    , PatternDefinition
     , Pos
+    , PositionedFragment
     , Side(..)
     , Socket
     , SocketSet
@@ -8,11 +11,53 @@ module SocketParser exposing
     , empty
     , parse
     , parseFragment
+    , parsePattern
     )
+
+import Dict exposing (Dict)
+import Maybe.Extra exposing (values)
 
 
 type Fragment
     = XFragment
+
+
+type alias PatternDefinition =
+    { positionMapping : Dict Char Pos
+    , lines : List String
+    }
+
+
+type alias PositionedFragment =
+    { patternPos : Pos
+    , socketPos : Pos
+    }
+
+
+type alias Pattern =
+    List PositionedFragment
+
+
+parsePattern : PatternDefinition -> Pattern
+parsePattern { positionMapping, lines } =
+    let
+        readPatterns : Int -> List Char -> List PositionedFragment
+        readPatterns row characters =
+            values <| List.indexedMap (readChar row) characters
+
+        readChar : Int -> Int -> Char -> Maybe PositionedFragment
+        readChar row column c =
+            case Dict.get c positionMapping of
+                Just pos ->
+                    Just
+                        { patternPos = ( column, row )
+                        , socketPos = pos
+                        }
+
+                Nothing ->
+                    Nothing
+    in
+    List.concat <| List.indexedMap readPatterns <| List.map String.toList lines
 
 
 parseFragment : String -> Result String Fragment
--- tests/SocketParserTest.elm
+++ tests/SocketParserTest.elm
@@ -1,13 +1,26 @@
 module SocketParserTest exposing (suite)
 
+import Dict
 import Expect
-import SocketParser exposing (Fragment(..), allSides, empty, parse, parseFragment)
+import SocketParser exposing (Fragment(..), Pattern, PatternDefinition, allSides, empty, parse, parseFragment, parsePattern)
 import Test exposing (Test, describe, test)
 
 
 example1d : String
 example1d =
     "| X |"
+
+
+examplePattern1d : String
+examplePattern1d =
+    "| a |"
+
+
+examplePattern1dDef : PatternDefinition
+examplePattern1dDef =
+    { positionMapping = Dict.fromList [ ( 'a', ( 1, 1 ) ) ]
+    , lines = [ examplePattern1d ]
+    }
 
 
 example3d : String
@@ -37,7 +50,22 @@ example3d =
 suite : Test
 suite =
     describe "The SocketParser module"
-        [ describe "the parse function"
+        [ describe "the parsePattern function"
+            [ test "works for the simple example" <|
+                \_ ->
+                    let
+                        expectedPattern : Pattern
+                        expectedPattern =
+                            [ { patternPos = ( 2, 0 )
+                              , socketPos = ( 1, 1 )
+                              }
+                            ]
+                    in
+                    Expect.equal
+                        expectedPattern
+                        (parsePattern examplePattern1dDef)
+            ]
+        , describe "the parse function"
             [ test "returns an empty SocketSet for the 1d example" <|
                 \_ ->
                     Expect.equal
