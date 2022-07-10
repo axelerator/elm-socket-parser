c1859b657d6978cfb97fbc2487ddd66717912d4e
Axel Tetzlaff <axel.tetzlaff@shopify.com>
2022-07-10 16:44:19 -0400

    Rotation working

--- src/SocketParser.elm
+++ src/SocketParser.elm
@@ -1,36 +1,63 @@
 module SocketParser exposing
-    ( Fragment(..)
+    ( Axis(..)
+    , Fragment(..)
     , Pattern
     , PatternDefinition
-    , Pos
+    , Pos(..)
     , PositionedFragment
     , Side(..)
     , Socket
     , SocketSet
     , allSides
-    , empty
+    , mkSocket
     , parse
     , parseFragment
     , parsePattern
+    , rotateSet
     )
 
 import Dict exposing (Dict)
 import Maybe.Extra exposing (values)
+import Result.Extra
+import String exposing (fromInt)
 
 
 type Fragment
-    = XFragment
+    = AFragment
+    | BFragment
+    | CFragment
+    | DFragment
+    | EFragment
+    | FFragment
+    | GFragment
+    | HFragment
+    | IFragment
+    | JFragment
+    | KFragment
+    | LFragment
+    | MFragment
+    | NFragment
+    | OFragment
+    | PFragment
+    | QFragment
+    | RFragment
+    | SFragment
+    | TFragment
+    | UFragment
+    | VFragment
+    | WFragment
+    | XFragment
 
 
 type alias PatternDefinition =
-    { positionMapping : Dict Char Pos
+    { positionMapping : Dict Char ( Side, Pos )
     , lines : List String
     }
 
 
 type alias PositionedFragment =
-    { patternPos : Pos
-    , socketPos : Pos
+    { patternPos : ( Int, Int )
+    , socketPos : ( Side, Pos )
     }
 
 
@@ -60,22 +87,115 @@ parsePattern { positionMapping, lines } =
     List.concat <| List.indexedMap readPatterns <| List.map String.toList lines
 
 
-parseFragment : String -> Result String Fragment
+parseFragment : Char -> Result String Fragment
 parseFragment s =
     case s of
-        "X" ->
+        'A' ->
+            Ok AFragment
+
+        'B' ->
+            Ok BFragment
+
+        'C' ->
+            Ok CFragment
+
+        'D' ->
+            Ok DFragment
+
+        'E' ->
+            Ok EFragment
+
+        'F' ->
+            Ok FFragment
+
+        'G' ->
+            Ok GFragment
+
+        'H' ->
+            Ok HFragment
+
+        'I' ->
+            Ok IFragment
+
+        'J' ->
+            Ok JFragment
+
+        'K' ->
+            Ok KFragment
+
+        'L' ->
+            Ok LFragment
+
+        'M' ->
+            Ok MFragment
+
+        'N' ->
+            Ok NFragment
+
+        'O' ->
+            Ok OFragment
+
+        'P' ->
+            Ok PFragment
+
+        'Q' ->
+            Ok QFragment
+
+        'R' ->
+            Ok RFragment
+
+        'S' ->
+            Ok SFragment
+
+        'T' ->
+            Ok TFragment
+
+        'U' ->
+            Ok UFragment
+
+        'V' ->
+            Ok VFragment
+
+        'W' ->
+            Ok WFragment
+
+        'X' ->
             Ok XFragment
 
         _ ->
-            Err <| "Unknown fragment: " ++ s
+            Err <| "Unknown fragment: " ++ String.fromList [ s ]
 
 
-type alias Pos =
-    ( Int, Int )
+type Pos
+    = P0
+    | P1
+    | P2
+    | P3
 
 
-type alias Socket =
-    List ( Pos, Fragment )
+posToComparable : Pos -> Int
+posToComparable p =
+    case p of
+        P0 ->
+            0
+
+        P1 ->
+            1
+
+        P2 ->
+            2
+
+        P3 ->
+            3
+
+
+type Socket
+    = Socket (List ( Pos, Fragment ))
+
+
+mkSocket : List ( Pos, Fragment ) -> Socket
+mkSocket ps =
+    Socket <| List.sortBy (\( p, _ ) -> posToComparable p) ps
 
 
 type Side
@@ -102,17 +222,348 @@ type alias SocketSet =
     }
 
 
-parse : String -> Result String SocketSet
-parse _ =
-    Ok empty
+parse : Pattern -> List String -> Result String SocketSet
+parse positionedFragments lines =
+    let
+        forSide : Side -> Socket
+        forSide side =
+            let
+                hasSide : ( ( Side, b ), a ) -> Bool
+                hasSide ( ( s, _ ), _ ) =
+                    s == side
+
+                dropSide : ( ( a, b ), c ) -> ( b, c )
+                dropSide ( ( _, pos ), fragment ) =
+                    ( pos, fragment )
+            in
+            mkSocket <| List.map dropSide <| List.filter hasSide parsedFragments
+
+        lookUp : PositionedFragment -> Result String ( ( Side, Pos ), Fragment )
+        lookUp { patternPos, socketPos } =
+            let
+                ( x, y ) =
+                    patternPos
+            in
+            case List.head <| List.drop y lines of
+                Just line ->
+                    case List.head <| List.drop x <| String.toList line of
+                        Just char ->
+                            case parseFragment char of
+                                Ok fragment ->
+                                    Ok ( socketPos, fragment )
+
+                                Err e ->
+                                    Err <| "At (" ++ fromInt x ++ "," ++ fromInt y ++ ")" ++ e
+
+                        Nothing ->
+                            Err "Out of bounds"
+
+                Nothing ->
+                    Err "Out of bounds"
+
+        parsedFragmentResults : List (Result String ( ( Side, Pos ), Fragment ))
+        parsedFragmentResults =
+            List.map lookUp positionedFragments
+
+        ( parsedFragments, errors ) =
+            Result.Extra.partition parsedFragmentResults
+    in
+    if List.isEmpty errors then
+        Ok
+            { top = forSide Top
+            , front = forSide Front
+            , bottom = forSide Bottom
+            , back = forSide Back
+            , left = forSide Left
+            , right = forSide Right
+            }
+
+    else
+        Err <| String.join ", " errors
 
 
-empty : SocketSet
-empty =
-    { top = []
-    , front = []
-    , bottom = []
-    , back = []
-    , left = []
-    , right = []
-    }
+type Axis
+    = XAxis
+    | YAxis
+    | ZAxis
+
+
+rotateSide : Axis -> Side -> Socket -> Socket
+rotateSide axis side (Socket ps) =
+    let
+        rotFunc : Side -> Pos -> Pos
+        rotFunc =
+            case axis of
+                XAxis ->
+                    rotateX
+
+                YAxis ->
+                    rotateY
+
+                ZAxis ->
+                    rotateZ
+
+        update : ( Pos, Fragment ) -> ( Pos, Fragment )
+        update ( pos, fragment ) =
+            ( rotFunc side pos, fragment )
+    in
+    mkSocket <| List.map update ps
+
+
+rotateSet : Axis -> SocketSet -> SocketSet
+rotateSet axis s =
+    case axis of
+        ZAxis ->
+            { top = rotateSide axis Top s.top
+            , front = rotateSide axis Left s.left
+            , bottom = rotateSide axis Bottom s.bottom
+            , back = rotateSide axis Right s.right
+            , left = rotateSide axis Back s.back
+            , right = rotateSide axis Front s.front
+            }
+
+        XAxis ->
+            { top = rotateSide axis Front s.front
+            , front = rotateSide axis Bottom s.bottom
+            , bottom = rotateSide axis Back s.back
+            , back = rotateSide axis Top s.top
+            , left = rotateSide axis Left s.left
+            , right = rotateSide axis Right s.right
+            }
+
+        YAxis ->
+            { top = rotateSide axis Right s.right
+            , front = rotateSide axis Front s.front
+            , bottom = rotateSide axis Left s.left
+            , back = rotateSide axis Back s.back
+            , left = rotateSide axis Top s.top
+            , right = rotateSide axis Bottom s.bottom
+            }
+
+
+rotateZ : Side -> Pos -> Pos
+rotateZ side pos =
+    case ( side, pos ) of
+        ( Top, P0 ) ->
+            P2
+
+        ( Top, P1 ) ->
+            P0
+
+        ( Top, P2 ) ->
+            P3
+
+        ( Top, P3 ) ->
+            P1
+
+        ( Bottom, P0 ) ->
+            P2
+
+        ( Bottom, P1 ) ->
+            P0
+
+        ( Bottom, P2 ) ->
+            P3
+
+        ( Bottom, P3 ) ->
+            P1
+
+        ( Front, P0 ) ->
+            P0
+
+        ( Front, P1 ) ->
+            P1
+
+        ( Front, P2 ) ->
+            P2
+
+        ( Front, P3 ) ->
+            P3
+
+        ( Back, P0 ) ->
+            P0
+
+        ( Back, P1 ) ->
+            P1
+
+        ( Back, P2 ) ->
+            P2
+
+        ( Back, P3 ) ->
+            P3
+
+        ( Left, P0 ) ->
+            P1
+
+        ( Left, P1 ) ->
+            P0
+
+        ( Left, P2 ) ->
+            P3
+
+        ( Left, P3 ) ->
+            P2
+
+        ( Right, P0 ) ->
+            P1
+
+        ( Right, P1 ) ->
+            P0
+
+        ( Right, P2 ) ->
+            P3
+
+        ( Right, P3 ) ->
+            P2
+
+
+rotateX : Side -> Pos -> Pos
+rotateX side pos =
+    case ( side, pos ) of
+        ( Top, P0 ) ->
+            P2
+
+        ( Top, P1 ) ->
+            P3
+
+        ( Top, P2 ) ->
+            P0
+
+        ( Top, P3 ) ->
+            P1
+
+        ( Bottom, P0 ) ->
+            P2
+
+        ( Bottom, P1 ) ->
+            P3
+
+        ( Bottom, P2 ) ->
+            P0
+
+        ( Bottom, P3 ) ->
+            P1
+
+        ( Front, P0 ) ->
+            P0
+
+        ( Front, P1 ) ->
+            P1
+
+        ( Front, P2 ) ->
+            P2
+
+        ( Front, P3 ) ->
+            P3
+
+        ( Back, P0 ) ->
+            P0
+
+        ( Back, P1 ) ->
+            P1
+
+        ( Back, P2 ) ->
+            P2
+
+        ( Back, P3 ) ->
+            P3
+
+        ( Left, P0 ) ->
+            P1
+
+        ( Left, P1 ) ->
+            P3
+
+        ( Left, P2 ) ->
+            P0
+
+        ( Left, P3 ) ->
+            P2
+
+        ( Right, P0 ) ->
+            P1
+
+        ( Right, P1 ) ->
+            P3
+
+        ( Right, P2 ) ->
+            P0
+
+        ( Right, P3 ) ->
+            P2
+
+
+rotateY : Side -> Pos -> Pos
+rotateY side pos =
+    case ( side, pos ) of
+        ( Top, P0 ) ->
+            P3
+
+        ( Top, P1 ) ->
+            P1
+
+        ( Top, P2 ) ->
+            P2
+
+        ( Top, P3 ) ->
+            P0
+
+        ( Bottom, P0 ) ->
+            P3
+
+        ( Bottom, P1 ) ->
+            P1
+
+        ( Bottom, P2 ) ->
+            P2
+
+        ( Bottom, P3 ) ->
+            P0
+
+        ( Front, P0 ) ->
+            P2
+
+        ( Front, P1 ) ->
+            P0
+
+        ( Front, P2 ) ->
+            P3
+
+        ( Front, P3 ) ->
+            P1
+
+        ( Back, P0 ) ->
+            P2
+
+        ( Back, P1 ) ->
+            P0
+
+        ( Back, P2 ) ->
+            P3
+
+        ( Back, P3 ) ->
+            P1
+
+        ( Left, P0 ) ->
+            P2
+
+        ( Left, P1 ) ->
+            P0
+
+        ( Left, P2 ) ->
+            P3
+
+        ( Left, P3 ) ->
+            P1
+
+        ( Right, P0 ) ->
+            P2
+
+        ( Right, P1 ) ->
+            P0
+
+        ( Right, P2 ) ->
+            P3
+
+        ( Right, P3 ) ->
+            P1
--- tests/SocketParserTest.elm
+++ tests/SocketParserTest.elm
@@ -2,7 +2,23 @@ module SocketParserTest exposing (suite)
 
 import Dict
 import Expect
-import SocketParser exposing (Fragment(..), Pattern, PatternDefinition, allSides, empty, parse, parseFragment, parsePattern)
+import SocketParser
+    exposing
+        ( Axis(..)
+        , Fragment(..)
+        , Pattern
+        , PatternDefinition
+        , Pos(..)
+        , Side(..)
+        , Socket
+        , SocketSet
+        , allSides
+        , mkSocket
+        , parse
+        , parseFragment
+        , parsePattern
+        , rotateSet
+        )
 import Test exposing (Test, describe, test)
 
 
@@ -18,33 +34,231 @@ examplePattern1d =
 
 examplePattern1dDef : PatternDefinition
 examplePattern1dDef =
-    { positionMapping = Dict.fromList [ ( 'a', ( 1, 1 ) ) ]
+    { positionMapping = Dict.fromList [ ( 'a', ( Top, P3 ) ) ]
     , lines = [ examplePattern1d ]
     }
 
 
-example3d : String
-example3d =
-    String.join "\n" <|
-        [ "          ▁▁▁▁▁▁    "
-        , "         ╱ X X ╱    "
-        , "        ╱ X X ╱     "
-        , "        ▔▔▔▔▔▔      "
-        , "          ┌───┐     "
-        , "    ╱ ▏   │X X│  ╱ ▏"
-        , "   ╱  ▏   │X X│ ╱  ▏"
-        , "  ╱ X ▏   └───┘╱ X ▏"
-        , " ╱  X╱        ╱  X╱ "
-        , " ▏X ╱┌───┐    ▏X ╱  "
-        , " ▏X╱ │X X│    ▏X╱   "
-        , " ▏╱  │X X│    ▏╱    "
-        , "     └───┘          "
-        , "     ▁▁▁▁▁▁         "
-        , "    ╱ X X ╱         "
-        , "   ╱ X X ╱          "
-        , "   ▔▔▔▔▔▔           "
-        , "                    "
+examplePattern3dDef : PatternDefinition
+examplePattern3dDef =
+    { positionMapping = example3dMapping
+    , lines = example3dPattern
+    }
+
+
+example3dMapping : Dict.Dict Char ( Side, Pos )
+example3dMapping =
+    Dict.fromList
+        [ ( 'a', ( Top, P0 ) )
+        , ( 'b', ( Top, P1 ) )
+        , ( 'c', ( Top, P2 ) )
+        , ( 'd', ( Top, P3 ) )
+        , ( 'e', ( Front, P0 ) )
+        , ( 'f', ( Front, P1 ) )
+        , ( 'g', ( Front, P2 ) )
+        , ( 'h', ( Front, P3 ) )
+        , ( 'i', ( Bottom, P0 ) )
+        , ( 'j', ( Bottom, P1 ) )
+        , ( 'k', ( Bottom, P2 ) )
+        , ( 'l', ( Bottom, P3 ) )
+        , ( 'm', ( Back, P0 ) )
+        , ( 'n', ( Back, P1 ) )
+        , ( 'o', ( Back, P2 ) )
+        , ( 'p', ( Back, P3 ) )
+        , ( 'q', ( Left, P0 ) )
+        , ( 'r', ( Left, P1 ) )
+        , ( 's', ( Left, P2 ) )
+        , ( 't', ( Left, P3 ) )
+        , ( 'u', ( Right, P0 ) )
+        , ( 'v', ( Right, P1 ) )
+        , ( 'w', ( Right, P2 ) )
+        , ( 'x', ( Right, P3 ) )
         ]
+
+
+example3dPattern : List String
+example3dPattern =
+    [ "          ▁▁▁▁▁▁    "
+    , "         ╱ a b ╱    "
+    , "        ╱ c d ╱     "
+    , "        ▔▔▔▔▔▔      "
+    , "          ┌───┐     "
+    , "    ╱ ▏   │m n│  ╱ ▏"
+    , "   ╱  ▏   │o p│ ╱  ▏"
+    , "  ╱ r ▏   └───┘╱ v ▏"
+    , " ╱  t╱        ╱  x╱ "
+    , " ▏q ╱┌───┐    ▏u ╱  "
+    , " ▏s╱ │e f│    ▏w╱   "
+    , " ▏╱  │g h│    ▏╱    "
+    , "     └───┘          "
+    , "     ▁▁▁▁▁▁         "
+    , "    ╱ i j ╱         "
+    , "   ╱ k l ╱          "
+    , "   ▔▔▔▔▔▔           "
+    , "                    "
+    ]
+
+
+example3d : List String
+example3d =
+    [ "          ▁▁▁▁▁▁    "
+    , "         ╱ A B ╱    "
+    , "        ╱ C D ╱     "
+    , "        ▔▔▔▔▔▔      "
+    , "          ┌───┐     "
+    , "    ╱ ▏   │A B│  ╱ ▏"
+    , "   ╱  ▏   │C D│ ╱  ▏"
+    , "  ╱ B ▏   └───┘╱ B ▏"
+    , " ╱  D╱        ╱  D╱ "
+    , " ▏A ╱┌───┐    ▏A ╱  "
+    , " ▏C╱ │A B│    ▏C╱   "
+    , " ▏╱  │C D│    ▏╱    "
+    , "     └───┘          "
+    , "     ▁▁▁▁▁▁         "
+    , "    ╱ A B ╱         "
+    , "   ╱ C D ╱          "
+    , "   ▔▔▔▔▔▔           "
+    , "                    "
+    ]
+
+
+example3dFull : List String
+example3dFull =
+    [ "          ▁▁▁▁▁▁    "
+    , "         ╱ A B ╱    "
+    , "        ╱ C D ╱     "
+    , "        ▔▔▔▔▔▔      "
+    , "          ┌───┐     "
+    , "    ╱ ▏   │M N│  ╱ ▏"
+    , "   ╱  ▏   │O P│ ╱  ▏"
+    , "  ╱ R ▏   └───┘╱ V ▏"
+    , " ╱  T╱        ╱  X╱ "
+    , " ▏Q ╱┌───┐    ▏U ╱  "
+    , " ▏S╱ │E F│    ▏W╱   "
+    , " ▏╱  │G H│    ▏╱    "
+    , "     └───┘          "
+    , "     ▁▁▁▁▁▁         "
+    , "    ╱ I J ╱         "
+    , "   ╱ K L ╱          "
+    , "   ▔▔▔▔▔▔           "
+    , "                    "
+    ]
+
+
+example3dFullRotatedY : List String
+example3dFullRotatedY =
+    [ "          ▁▁▁▁▁▁    "
+    , "         ╱ V X ╱    "
+    , "        ╱ U W ╱     "
+    , "        ▔▔▔▔▔▔      "
+    , "          ┌───┐     "
+    , "    ╱ ▏   │N P│  ╱ ▏"
+    , "   ╱  ▏   │M O│ ╱  ▏"
+    , "  ╱ B ▏   └───┘╱ J ▏"
+    , " ╱  A╱        ╱  I╱ "
+    , " ▏D ╱┌───┐    ▏L ╱  "
+    , " ▏C╱ │F H│    ▏K╱   "
+    , " ▏╱  │E G│    ▏╱    "
+    , "     └───┘          "
+    , "     ▁▁▁▁▁▁         "
+    , "    ╱ R T ╱         "
+    , "   ╱ Q S ╱          "
+    , "   ▔▔▔▔▔▔           "
+    , "                    "
+    ]
+
+
+example3dFullRotatedX : List String
+example3dFullRotatedX =
+    [ "          ▁▁▁▁▁▁    "
+    , "         ╱ E F ╱    "
+    , "        ╱ G H ╱     "
+    , "        ▔▔▔▔▔▔      "
+    , "          ┌───┐     "
+    , "    ╱ ▏   │C D│  ╱ ▏"
+    , "   ╱  ▏   │A B│ ╱  ▏"
+    , "  ╱ Q ▏   └───┘╱ U ▏"
+    , " ╱  R╱        ╱  V╱ "
+    , " ▏S ╱┌───┐    ▏W ╱  "
+    , " ▏T╱ │K L│    ▏X╱   "
+    , " ▏╱  │I J│    ▏╱    "
+    , "     └───┘          "
+    , "     ▁▁▁▁▁▁         "
+    , "    ╱ M N ╱         "
+    , "   ╱ O P ╱          "
+    , "   ▔▔▔▔▔▔           "
+    , "                    "
+    ]
+
+
+example3dFullRotatedZ : List String
+example3dFullRotatedZ =
+    [ "          ▁▁▁▁▁▁    "
+    , "         ╱ B D ╱    "
+    , "        ╱ A C ╱     "
+    , "        ▔▔▔▔▔▔      "
+    , "          ┌───┐     "
+    , "    ╱ ▏   │V U│  ╱ ▏"
+    , "   ╱  ▏   │X W│ ╱  ▏"
+    , "  ╱ N ▏   └───┘╱ F ▏"
+    , " ╱  P╱        ╱  H╱ "
+    , " ▏M ╱┌───┐    ▏E ╱  "
+    , " ▏O╱ │R Q│    ▏G╱   "
+    , " ▏╱  │T S│    ▏╱    "
+    , "     └───┘          "
+    , "     ▁▁▁▁▁▁         "
+    , "    ╱ J L ╱         "
+    , "   ╱ I K ╱          "
+    , "   ▔▔▔▔▔▔           "
+    , "                    "
+    ]
+
+
+expectedSocketSet : SocketSet
+expectedSocketSet =
+    { top =
+        mkSocket
+            [ ( P0, AFragment )
+            , ( P1, BFragment )
+            , ( P2, CFragment )
+            , ( P3, DFragment )
+            ]
+    , front =
+        mkSocket
+            [ ( P0, AFragment )
+            , ( P1, BFragment )
+            , ( P2, CFragment )
+            , ( P3, DFragment )
+            ]
+    , bottom =
+        mkSocket
+            [ ( P0, AFragment )
+            , ( P1, BFragment )
+            , ( P2, CFragment )
+            , ( P3, DFragment )
+            ]
+    , back =
+        mkSocket
+            [ ( P0, AFragment )
+            , ( P1, BFragment )
+            , ( P2, CFragment )
+            , ( P3, DFragment )
+            ]
+    , left =
+        mkSocket
+            [ ( P1, BFragment )
+            , ( P3, DFragment )
+            , ( P0, AFragment )
+            , ( P2, CFragment )
+            ]
+    , right =
+        mkSocket
+            [ ( P1, BFragment )
+            , ( P3, DFragment )
+            , ( P0, AFragment )
+            , ( P2, CFragment )
+            ]
+    }
 
 
 suite : Test
@@ -57,25 +271,102 @@ suite =
                         expectedPattern : Pattern
                         expectedPattern =
                             [ { patternPos = ( 2, 0 )
-                              , socketPos = ( 1, 1 )
+                              , socketPos = ( Top, P3 )
                               }
                             ]
                     in
                     Expect.equal
                         expectedPattern
                         (parsePattern examplePattern1dDef)
+            , test "works for the 3d example" <|
+                \_ ->
+                    Expect.equal
+                        24
+                        (List.length (parsePattern examplePattern3dDef))
             ]
         , describe "the parse function"
-            [ test "returns an empty SocketSet for the 1d example" <|
+            [ test "can parse the 1d" <|
                 \_ ->
-                    Expect.equal
-                        (Ok empty)
-                        (parse example1d)
-            , test "returns an empty SocketSet for the 3d example" <|
+                    let
+                        pattern : Pattern
+                        pattern =
+                            parsePattern examplePattern1dDef
+                    in
+                    Expect.ok
+                        (parse pattern [ example1d ])
+            , test "can parse the 3d example" <|
                 \_ ->
-                    Expect.equal
-                        (Ok empty)
-                        (parse example3d)
+                    let
+                        pattern : Pattern
+                        pattern =
+                            parsePattern examplePattern3dDef
+                    in
+                    Expect.ok
+                        (parse pattern example3d)
+            , describe "the parsed fragments"
+                [ test "match on the top" (expectMatch Top)
+                , test "match on the front" (expectMatch Front)
+                , test "match on the bottom" (expectMatch Bottom)
+                , test "match on the back" (expectMatch Back)
+                , test "match on the left" (expectMatch Left)
+                , test "match on the right" (expectMatch Right)
+                ]
+            ]
+        , describe "the rotateSet function"
+            [ test "returns expected result when rotating around Z axis" <|
+                \_ ->
+                    let
+                        pattern : Pattern
+                        pattern =
+                            parsePattern examplePattern3dDef
+                    in
+                    case ( parse pattern example3dFull, parse pattern example3dFullRotatedZ ) of
+                        ( Ok input, Ok expected ) ->
+                            Expect.equal
+                                expected
+                                (rotateSet ZAxis input)
+
+                        ( Err e, _ ) ->
+                            Expect.fail <| "Unable to parse input " ++ e
+
+                        ( _, Err e ) ->
+                            Expect.fail <| "Unable to parse expected " ++ e
+            , test "returns expected result when rotating around X axis" <|
+                \_ ->
+                    let
+                        pattern : Pattern
+                        pattern =
+                            parsePattern examplePattern3dDef
+                    in
+                    case ( parse pattern example3dFull, parse pattern example3dFullRotatedX ) of
+                        ( Ok input, Ok expected ) ->
+                            Expect.equal
+                                expected
+                                (rotateSet XAxis input)
+
+                        ( Err e, _ ) ->
+                            Expect.fail <| "Unable to parse input " ++ e
+
+                        ( _, Err e ) ->
+                            Expect.fail <| "Unable to parse expected " ++ e
+            , test "returns expected result when rotating around Y axis" <|
+                \_ ->
+                    let
+                        pattern : Pattern
+                        pattern =
+                            parsePattern examplePattern3dDef
+                    in
+                    case ( parse pattern example3dFull, parse pattern example3dFullRotatedY ) of
+                        ( Ok input, Ok expected ) ->
+                            Expect.equal
+                                expected
+                                (rotateSet YAxis input)
+
+                        ( Err e, _ ) ->
+                            Expect.fail <| "Unable to parse input " ++ e
+
+                        ( _, Err e ) ->
+                            Expect.fail <| "Unable to parse expected " ++ e
             ]
         , describe "the allSides function"
             [ test "returns six sides" <|
@@ -88,7 +379,46 @@ suite =
                     \_ ->
                         Expect.equal
                             (Ok XFragment)
-                            (parseFragment "X")
+                            (parseFragment 'X')
                 ]
             ]
         ]
+
+
+parsed3dExample : Result String SocketSet
+parsed3dExample =
+    parse (parsePattern examplePattern3dDef) example3d
+
+
+expectMatch : Side -> () -> Expect.Expectation
+expectMatch side _ =
+    case parsed3dExample of
+        Ok sides ->
+            let
+                accessor : SocketSet -> Socket
+                accessor =
+                    case side of
+                        Top ->
+                            .top
+
+                        Front ->
+                            .front
+
+                        Bottom ->
+                            .bottom
+
+                        Back ->
+                            .back
+
+                        Left ->
+                            .left
+
+                        Right ->
+                            .right
+            in
+            Expect.equal
+                (accessor expectedSocketSet)
+                (accessor sides)
+
+        Err e ->
+            Expect.fail e
